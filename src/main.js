8c93fce6fa89d51bef4831dcf1c4494fee87165f
Tugkan Cengiz <tugkancengiz@gmail.com>
2020-03-15 13:19:09 +0300

    linting

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ const Apify = require('apify');
 
 const { log } = Apify.utils;
 const sourceUrl = 'https://www.folkhalsomyndigheten.se/smittskydd-beredskap/utbrott/aktuella-utbrott/covid-19/aktuellt-epidemiologiskt-lage/';
-const LATEST = "LATEST";
+const LATEST = 'LATEST';
 
 Apify.main(async () => {
     const requestQueue = await Apify.openRequestQueue();
     const kvStore = await Apify.openKeyValueStore('COVID-19-SWEDEN');
-    const dataset = await Apify.openDataset("COVID-19-SWEDEN-HISTORY");
+    const dataset = await Apify.openDataset('COVID-19-SWEDEN-HISTORY');
 
     await requestQueue.addRequest({ url: sourceUrl });
     const crawler = new Apify.CheerioCrawler({
@@ -17,43 +17,43 @@ Apify.main(async () => {
         handlePageTimeoutSecs: 60 * 2,
         handlePageFunction: async ({ $ }) => {
             log.info('Page loaded.');
-            const now = new Date();            
+            const now = new Date();
 
-            const rows = $($("#content-primary table")[0]).find("tr").get()
+            const rows = $($('#content-primary table')[0]).find('tr').get();
             rows.shift(); // remove title
             const totalRow = rows.pop();
-            
-            const infectedByRegion = rows.map(r => {
-                const columns = $(r).find("td")
-                const region = $(columns[0]).text()
-                const infectedCount = parseInt($(columns[1]).text())
+
+            const infectedByRegion = rows.map((r) => {
+                const columns = $(r).find('td');
+                const region = $(columns[0]).text();
+                const infectedCount = parseInt($(columns[1]).text());
                 return {
                     region,
-                    infectedCount
-                }                
+                    infectedCount,
+                };
             });
-            
+
             const data = {
-                infected: $($(totalRow).find("td")[1]).text(),
+                infected: $($(totalRow).find('td')[1]).text(),
                 infectedByRegion,
                 sourceUrl,
                 lastUpdatedAtApify: new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours() + 1, now.getMinutes())).toISOString(),
                 readMe: 'https://apify.com/tugkan/covid-se',
-            }
-            
+            };
+
             // Compare and save to history
             const latest = await kvStore.getValue(LATEST) || {};
             delete latest.lastUpdatedAtApify;
             const actual = Object.assign({}, data);
             delete actual.lastUpdatedAtApify;
 
-            if(JSON.stringify(latest)!== JSON.stringify(actual)){
-                log.info('Data did change :( storing new to dataset.')
+            if (JSON.stringify(latest) !== JSON.stringify(actual)) {
+                log.info('Data did change :( storing new to dataset.');
                 await dataset.pushData(data);
             }
 
             await kvStore.setValue(LATEST, data);
-            log.info('Data stored, finished.')
+            log.info('Data stored, finished.');
         },
 
         // This function is called if the page processing failed more than maxRequestRetries+1 times.
